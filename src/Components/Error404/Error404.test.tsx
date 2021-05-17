097cfd119d31a32b4038cf5f21cbd2b0c8fe10b9
Germán Aliprandi <galiprandi@gmail.com>
2021-05-17 05:21:55 -0300

    Add: 404 page test

---
+++ src/Components/Error404/Error404.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react'
+
+// Import component
+import { Error404 as Component } from './Error404'
+
+// Render Component
+beforeEach(() => render(<Component />))
+
+describe('<Error404 />', () => {
+  it('should render a 404 page', () => {
+    screen.getByRole('heading', { name: /found/i })
+  })
+})
